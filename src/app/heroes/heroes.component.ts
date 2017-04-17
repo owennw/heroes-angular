38c8f9f0b59c4837f90064f59b6d3acc093074d6
Nick Owen <nowen@scottlogic.co.uk>
2017-04-17 17:54:44 +0100

    feat: navigate from heroes to details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 import Hero from './hero'
 import HeroService from './hero.service'
@@ -13,7 +14,10 @@ export default class Heroes implements OnInit {
   heroes: Hero[]
   selectedHero: Hero
 
-  constructor(private heroService: HeroService) {
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) {
   }
 
   ngOnInit(): void {
@@ -27,5 +31,9 @@ export default class Heroes implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id])
   }
 }
